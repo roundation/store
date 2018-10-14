b3c694509e056f1b9af04e555379a7c93e3f6a5c
Zheeeng <hi@zheeeng.me>
2018-10-14 17:36:10 +0800

    Add util types: OmitType

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,3 @@
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+
+export type OmitType<B, C> = Pick<B, Exclude<keyof B, { [k in keyof B]: B[k] extends C ? k : never }[keyof B]>>
