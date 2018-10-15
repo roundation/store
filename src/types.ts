c1a3f41dd82db12710faae32eebe72c055965041
Zheeeng <hi@zheeeng.me>
2018-10-15 20:58:48 +0800

    Add XOR type

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,7 @@ export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 export type PickByType<B, C> = Pick<B, { [k in keyof B]: B[k] extends C ? k : never }[keyof B]>
 
 export type OmitByType<B, C> = Pick<B, Exclude<keyof B, { [k in keyof B]: B[k] extends C ? k : never }[keyof B]>>
+
+export type XOR<T, U> = (T | U) extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U
+
+export type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never }
